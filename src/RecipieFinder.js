aec630d8cd876fb311e0d769b06bea3295615aa5
atulvinod <atulvinod1911@gmail.com>
2019-07-28 12:01:17 +0530

    Implemented The Search Functionality

--- src/RecipieFinder.js
+++ src/RecipieFinder.js
@@ -1,9 +1,34 @@
 import React,{Component} from 'react';
 import RecipieSearch from './RecipieSearch'
+import SearchResultDisplay from './SearchResultDisplay';
+import $ from 'jquery';
 class RecipieFinder extends Component{
+
+    constructor(){
+        super();
+        this.state = {};
+    }
+    fetchResult = (query)=>{
+        
+        let request = new XMLHttpRequest();
+        request.open('GET','https://www.themealdb.com/api/json/v1/1/search.php?s='+query);
+        request.onreadystatechange = function(){
+            if(this.readyState==4){
+                console.log(this.responseText)
+            }
+        }
+        request.send();
+    }
+
+
     render(){
         return(
-            <RecipieSearch/>
+            <div>
+                <RecipieSearch fetchResult={this.fetchResult}/>
+                <SearchResultDisplay/>
+            </div>
+            
+
         );
     }
 
--- src/RecipieSearch.js
+++ src/RecipieSearch.js
@@ -1,13 +1,27 @@
 import React,{Component} from 'react';
 import './css/RecipieSearch.css'
 class RecipieSearch extends Component{
+constructor(){
+    super();
+    this.state = {search:""};
+}
+getResult = (event)=>{
+    event.preventDefault();
+}
+setQuery = (event)=>{
+    const state = this.state;
+    state['search'] = event.target.value;
+    this.setState(state);
+    console.log(this.state);
+}
+
 render(){
     return(
         <div>
             <h4>Recipie Finder</h4>
             <div>
-                <form>
-                    <input name="search" id="search" placeholder="Enter the name of the dish"/>
+                <form onSubmit={this.getResult}>
+                    <input name="search" id="search" placeholder="Enter the name of the dish" onChange={this.setQuery}/>
                     <button> Get Ingredients</button>
                 </form>
             </div>
